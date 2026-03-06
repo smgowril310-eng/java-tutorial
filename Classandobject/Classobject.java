d73110bd3bfd4bae87669e006c8d033bf5b64703
Gowri <smgowril310@gmail.com>
2026-03-06 11:35:52 -0500

    Class & Object are committed

---
+++ Classandobject/Classobject.java
@@ -0,0 +1,11 @@
+package Classandobject;
+//class - blueprint of object     
+public class Classobject {   
+    String name="Arun";
+
+public static void main(String[]args){
+    //object -instances of class
+    Classobject object=new Classobject();
+    System.out.print(object.name);
+}
+}
---
+++ Classandobject/SampleExplain.java
@@ -0,0 +1,13 @@
+package Classandobject;
+
+public class SampleExplain { //SampleExplain - Userdefined class
+    public static void main(String[] args) {  //public - Predefined class , String[]args - Parameter
+      System.out.println("Java");  //System - class , out - reference , println - method
+    }
+}
+
+/* public - Access modifier
+   static - keyword(memory allocation while class loading)
+   void -  non return type
+   main - method name
+   string[] - can give multiple string inputs */
