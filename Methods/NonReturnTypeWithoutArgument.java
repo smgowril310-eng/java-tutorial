586e88623f40252bdff0b1c3e0944f869abd7afb
Gowri <smgowril310@gmail.com>
2026-03-09 12:23:35 -0400

    Methods are committed

--- Methods/NonReturnTypeWithoutArgument.java
+++ Methods/NonReturnTypeWithoutArgument.java
@@ -1,6 +1,6 @@
 package Methods;
 
-public class NonReturnTypeWithoutParameter {
+public class NonReturnTypeWithoutArgument {
     public void add(){
         int a=10;
         int b=20;
@@ -9,7 +9,7 @@ public class NonReturnTypeWithoutParameter {
     public static void main(String[] args) {
         System.out.println(Math.max(20, 10));
         System.out.println(Math.min(20, 10));
-        NonReturnTypeWithoutParameter obj=new NonReturnTypeWithoutParameter();
+        NonReturnTypeWithoutArgument obj=new NonReturnTypeWithoutArgument();
        obj.add();
     }
 }
